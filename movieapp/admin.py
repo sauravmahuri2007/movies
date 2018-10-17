390c910717f3989370a8d8e69edf508ba8868e33
Saurav Kumar <saur.k10@gmail.com>
2018-10-17 18:34:12 +0530

    Registering the Models to admin.site

--- movieapp/admin.py
+++ movieapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Person, Movie, Role, Cast, Genre
+
 # Register your models here.
+
+admin.site.register(Role)
+admin.site.register(Person)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(Cast)
